c07e538e15397ad642ea24f0b18d2fdd470f37ab
aju007ak <john.ajith95ak@gmail.com>
2018-12-22 10:50:31 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -2,7 +2,7 @@ public class Largest {
 
     public static void main(String[] args) {
 
-        double n1 = -4.5, n2 = 3.9, n3 = 2.5;
+        double n1 = -3.0, n2 = 2.3, n3 = 4.5;
 
         if( n1 >= n2 && n1 >= n3)
             System.out.println(n1 + " is the largest number.");
