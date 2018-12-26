855ff4ddfbb18d3441413aa72b36cdb9fe7e6c00
aju007ak <john.ajith95ak@gmail.com>
2018-12-26 14:42:49 +0530

    Create ki.py

---
+++ ki.py
@@ -0,0 +1,21 @@
+a = 7
+
+factorial = 1
+if a< 0:
+   
+print("Sorry")
+
+elif 
+a == 0:
+   
+print("The factorial of 0 is 1")
+
+else:
+   
+for i in range(1,a+ 1):
+       
+factorial = factorial*i
+   
+print("The factorial of",a,"is",factorial)
+
+}
