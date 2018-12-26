e09360317f42dfb9970a959cce10ff98094abeb3
aju007ak <john.ajith95ak@gmail.com>
2018-12-26 14:46:47 +0530

    Create ak.java

---
+++ ak.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ak {
+   public static void main (String [] args) {
+
+      int a = 0;
+      int b = 0;
+
+      Scanner input = new Scanner(System.in);
+
+         System.out.print("Enter a: ");
+         a = input.nextInt();
+         System.out.print("Enter b: ");
+         b = input.nextInt();
+
+         if ((a <= 1) || (b <= 1)) {
+            System.out.println("Please enter numbers above 1");
+            System.exit(1);
+         }
+
+         System.out.print(b + " multiples of " + a + " are: ");
+
+   }
+}
