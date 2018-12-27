023f50434f10bdefbed793a0f62b48233a165fe6
aju007ak <john.ajith95ak@gmail.com>
2018-12-27 19:37:32 +0530

    Update ceee.c

--- ceee.c
+++ ceee.c
@@ -1,21 +1,21 @@
 #include <stdio.h> 
   
 
-int largest(int arr[], int a) 
+int largest(int a[], int b) 
 { 
- int i; 
-  int max = arr[0];   
- for (i = 1; i < a; i++) 
-        if (arr[i] > max) 
-            max = arr[i]; 
+ int r; 
+  int m = a[0];   
+ for (r = 1; r < b; i++) 
+        if (a[i] > m) 
+            m = a[i]; 
   
-    return max; 
+    return m; 
 } 
   
 int main() 
 { 
-    int arr[] = {10, 324, 45, 90, 9808}; 
-    int a = sizeof(arr)/sizeof(arr[0]); 
-    printf("Largest in given array is %d", largest(arr, a)); 
+    int a[] = {10, 324, 45, 90, 9808}; 
+    int b = sizeof(a)/sizeof(a[0]); 
+    printf("Largest in given array is %d", largest(a, b)); 
     return 0; 
 } 
