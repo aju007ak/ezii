911ce13cf2ba11059b3caba688ad0e8a786edadb
aju007ak <john.ajith95ak@gmail.com>
2018-12-27 20:51:20 +0530

    Create sddd.py

---
+++ sddd.py
@@ -0,0 +1,11 @@
+str=raw_input("Enter string:")
+d=0
+a=1
+for b in string:
+d=d+1
+if(b==' '):
+a=a+1
+print("Number of words in the string:")
+print(a)
+print("Number of characters in the string:")
+print(d)
