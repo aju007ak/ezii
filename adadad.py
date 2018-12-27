f67af3296c23b1699c8577a6090d90751e4389b6
aju007ak <john.ajith95ak@gmail.com>
2018-12-27 22:00:52 +0530

    Create adadad.py

---
+++ adadad.py
@@ -0,0 +1,12 @@
+def printFibonacciNumbers(e): 
+a1 = 0
+b2 = 1
+if (e < 1): 
+return
+for y in range(0, n): 
+print(b2, end = " ") 
+next = a1 + b2 
+a1 = b2 
+b2 = next
+printFibonacciNumbers(8) 
+  
