a622e28571677a3ead791b7902f3f867e5dd996a
aju007ak <john.ajith95ak@gmail.com>
2018-12-27 22:40:41 +0530

    Create sfsfg.java

---
+++ sfsfg.java
@@ -0,0 +1,16 @@
+class Avg
+{
+public static void main(String args[])
+{
+long a,b;
+int n;
+double r=0;   
+a=args.len;
+for(n=0;n<a;n++)
+r=r+Integer.parseInt(args[i]);	
+System.out.print("Average of (");
+for(n=0;n<a-1;n++)
+System.out.print(args[n]+",");
+System.out.println(args[n]+")="+r/a);
+}
+}
