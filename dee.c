440321001cee38747277436a564558ee597a3e78
aju007ak <john.ajith95ak@gmail.com>
2018-12-27 19:31:26 +0530

    Create dee.c

---
+++ dee.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int array[100], a,b, c, d = 1;
+   
+    printf("Enter number of elements in array\n");
+    scanf("%d", &b);
+   
+    printf("Enter %d integers\n",b);
+   
+    for (c = 0; c < b; c++)
+        scanf("%d", &array[c]);
+   
+    minimum = array[0];
+   
+    for (c = 1; c < b; c++)
+    {
+        if (array[c] < a)
+        {
+          a = array[c];
+           d = c+1;
+        }
+    }
+   
+    printf("Min element is present at loc %d and its value is %d.\n", d, a);
+    return 0;
+}
