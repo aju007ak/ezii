97ce92306e418447ab28181a78f126f5aa45fd0f
aju007ak <john.ajith95ak@gmail.com>
2018-12-27 20:14:16 +0530

    Update swwww.java

--- swwww.java
+++ swwww.java
@@ -1,20 +1,49 @@
-import java.io.*; 
-import java.util.*; 
-public class Arr
+class QuickSort 
 { 
-static void sortit(int []a, int b) 
+int a(int arr[], int a, int b) 
 { 
-for (int i = 0; i < b; i++)  
+int p = arr[b];  
+int i = (a-1);
+for (int j=a; j<b; j++) 
 { 
-a[i]=i+1; 
+if (arr[j] <= p) 
+{ 
+i++; 
+int t= arr[i]; 
+arr[i] = arr[j]; 
+arr[j] = t; 
+} 
+}
+int t = arr[i+1]; 
+arr[i+1] = arr[b]; 
+arr[b] = a; 
+return i+1; 
+} 
+void sort(int arr[], int a, int b) 
+{ 
+if (a< b) 
+{ 
+int z = p(arr, a, b); 
+sort(arr,a, z-1); 
+sort(arr, z+1,b); 
 } 
 } 
-public static void main(String args[]) 
-{
-int []a= {10, 7, 9, 2, 8,3, 5, 4, 6, 1}; 
-int b = a.length; 
-sortit(a,b); 
-for (int i = 0; i < b; i++)  
-System.out.print(a[i] + " ");  
+static void printArray(int arr[]) 
+{ 
+int f = arr.length; 
+for (int i=0; i<f; ++i) 
+System.out.print(arr[i]+" "); 
+System.out.println(); 
+} 
+    public static void main(String args[]) 
+    { 
+        int arr[] = {10, 7, 8, 9, 1, 5}; 
+        int f = arr.length; 
+  
+        QuickSort ob = new QuickSort(); 
+        ob.sort(arr, 0, f-1); 
+  
+        System.out.println("sorted array"); 
+        printArray(arr); 
     } 
 } 
