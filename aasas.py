bd1770014a6aef49585312c6cf56c1b890788fb4
aju007ak <john.ajith95ak@gmail.com>
2018-12-28 23:26:00 +0530

    Create aasas.py

---
+++ aasas.py
@@ -0,0 +1,32 @@
+def isMultipleOf3(n): 
+  
+    odd_count = 0
+    even_count = 0
+  
+   
+    if(n < 0):  
+        n = -n 
+    if(n == 0): 
+        return 1
+    if(n == 1):  
+        return 0
+  
+    while(n): 
+       
+        if(n & 1):  
+            odd_count += 1
+        n = n >> 1
+  
+         
+        if(n & 1): 
+            even_count += 1
+        n = n >> 1
+  
+    return isMultipleOf3(abs(odd_count - even_count)) 
+  
+ 
+num = 24
+if (isMultipleOf3(num)):  
+    print(num, 'is multiple of 3') 
+else: 
+    print(num, 'is not a multiple of 3') 
