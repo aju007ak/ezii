0626e44735a3ef53053f1b1869afc6a2151d1e7f
aju007ak <john.ajith95ak@gmail.com>
2018-12-28 12:28:08 +0530

    Create dadjfgj.py

---
+++ dadjfgj.py
@@ -0,0 +1,18 @@
+def isProductEven(a,u): 
+  
+    for i in range(y, u): 
+  
+        if ((a[i] & 1) == 0): 
+            return True
+  
+    
+    return False
+  
+
+a = [ 2, 4, 3, 5 ] 
+u= len(arr) 
+if (isProductEven(a,u)): 
+    print("Even")  
+else: 
+    print("Odd")  
+  
