21e653b82eff8929ca8af9072028f95c4fc30bee
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 11:23:13 +0530

    Update rr6666666666666.py

--- rr6666666666666.py
+++ rr6666666666666.py
@@ -1,14 +1,14 @@
 import sys,string, math, itertools
 
-s1, s2 = input().split()
-n = len(s1)
+a1, a2 = input().split()
+n = len(a1)
 for j in range(1,0,-1) :
     #print('arr len = ', j+1)
     for i in range(0,n-j) :
         li, ri = i,j+i
-        s3 = s1[li:ri + 1]
-        #print(li, ri, s3)
-        if s3 in s2 :
+        a3 = a1[li:ri + 1]
+        #print(li, ri, a3)
+        if a3 in a2 :
             print('yes')
             sys.exit()
 print('no')
