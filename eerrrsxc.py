ab4814bdea9e8d7818efc63975b7238125c188e7
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 11:19:33 +0530

    Update eerrrsxc.py

--- eerrrsxc.py
+++ eerrrsxc.py
@@ -1,20 +1,18 @@
-def knapSack(W , wt , val , n): 
+import sys,string, math, itertools
+from itertools import permutations,combinations
 
-    if n == 0 or W == 0 : 
-        return 0
-  
-    
-    if (wt[n-1] > W): 
-        return knapSack(W , wt , val , n-1) 
-  
-    else: 
-        return max(val[n-1] + knapSack(W-wt[n-1] , wt , val , n-1), 
-                   knapSack(W , wt , val , n-1)) 
- 
-  
-
-val = [60, 100, 120] 
-wt = [10, 20, 30] 
-W = 50
-n = len(val) 
-print knapSack(W , wt , val , n) 
+n,k = input().split()
+n,k = int(n),int(k)
+L1 = [ int(x) for x in input().split()]
+#print(n,k, L1)
+L2 = list(combinations(L1,k))
+#print(L2)
+L2 = list(itertools.combinations_with_replacement(L1,k))
+#print(L2)
+L3 = []
+for x in L2 :
+    sum1 = sum(x)
+    if sum1 not in L3 :
+        L3.append(sum1)
+L3.sort()
+print(*L3)) 
