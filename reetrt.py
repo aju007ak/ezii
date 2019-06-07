5917d7fe0bd25ea6bb77b6d6cecd6f2cc9ba215a
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 11:16:16 +0530

    Update reetrt.py

--- reetrt.py
+++ reetrt.py
@@ -1,9 +1,9 @@
 import sys, string, math
 s = input()
 s = s.lower()
-s2 = string.ascii_lowercase
-#print(s,s2)
-for c in s2 :
+a2 = string.ascii_lowercase
+#print(s,a2)
+for c in a2 :
     if c not in s :
         print('no')
         sys.exit()
