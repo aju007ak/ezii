f6ef7fa5640aa708b83691112a439cd3ae98bf00
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 11:18:25 +0530

    Update ytytygfd.py

--- ytytygfd.py
+++ ytytygfd.py
@@ -1,40 +1,17 @@
-def first( arr, low, high): 
-    if high >= low: 
-          
- 
-        mid = low + (high - low)//2
-  
-        if (mid == 0 or arr[mid - 1] == 0) and arr[mid] == 1: 
-            return mid 
-  
-        
-        elif arr[mid] == 0: 
-            return first(arr, (mid + 1), high) 
-      
-     
-        else: 
-            return first(arr, low, (mid - 1)) 
-    return -1
-  
-def rowWithMax1s( mat): 
-      
-    R = len(mat) 
-    C = len(mat[0]) 
-    max_row_index = 0
-    max = -1
-      
-   
-    for i in range(0, R): 
-        index = first (mat[i], 0, C - 1) 
-        if index != -1 and C - index > max: 
-            max = C - index 
-            max_row_index = i 
-  
-    return max_row_index 
+import sys,string
 
-mat = [[0, 0, 0, 1], 
-       [0, 1, 1, 1], 
-       [1, 1, 1, 1], 
-       [0, 0, 0, 0]] 
-print ("Index of row with maximum 1s is",  
-      rowWithMax1s(mat)) 
+n,k = input().split()
+n,k = int(n),int(k)
+L1 = [ int(x) for x in input().split()]
+L2 = [ int(x) for x in input().split()]
+max1 = 0
+for i in range(0,n) :
+    L3 = L2[:]
+    L3[i] += k
+    L4 = []
+    for j in range(0,n) :
+        L4.append(L3[j]//L1[j])
+    min1 = min(L4)
+    if min1 > max1 :
+        max1 = min1
+print(max1)
